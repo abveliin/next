0e408efa2671ee790023ad48c692db8cc5edec40
Avélin mz <abvelin.mz@gmail.com>
2023-01-09 00:13:52 +0200

    afafa

--- pages/post/[id].ts
+++ pages/post/[id].ts
@@ -1,37 +1,39 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import {prisma} from "../../../lib/prisma"
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../lib/prisma";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const post_id = req.query.id;
 
-    const post_id = req.query.id
+  if (req.method === "PUT") {
+    const { title, content } = req.body;
 
-    if(req.method==='PUT'){
-      const {title, content} = req.body
-
-      try {
-        await prisma.post.update({
-          where: {id: String(post_id)},
+    try {
+      await prisma.post
+        .update({
+          where: { id: String(post_id) },
           data: {
             title: title,
-            content: content
-          }
-        }).then(res => () => console.log(res))
-      } catch (error) {
-        console.log("failure")  
-      }
+            content: content,
+          },
+        })
+        .then((res) => () => console.log(res));
+    } catch {
+      console.log("failure");
     }
-    if(req.method === 'DELETE'){
-      try {
-        await prisma.post.delete({
-        where: {id: String(post_id)}
-      }).then(res => () => console.log(res))
-      }catch (error) {
-        console.log(error)  
-      }
+  }
+  if (req.method === "DELETE") {
+    try {
+      await prisma.post
+        .delete({
+          where: { id: String(post_id) },
+        })
+        .then((res) => () => console.log(res));
+    } catch {
+      console.log("error");
     }
-    
+  }
 }
